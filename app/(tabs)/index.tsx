fee29752321a081a4a13329d3ac03a39777d9dc5
Ashwin Poefra <kaspersky3550879@gmail.com>
2024-11-07 18:33:00 +0900

    feat: add ImageViwer component

--- app/(tabs)/index.tsx
+++ app/(tabs)/index.tsx
@@ -1,13 +1,14 @@
 import { View, StyleSheet } from 'react-native';
-import { Image } from 'expo-image';
+
+import ImageViewer from '../../components/ImageViewer';
 
 const PlaceholderImage = require('@/assets/images/background-image.png');
 
 export default function Index() {
   return (
-    <View style={styles.container}>
-      <View style={styles.imageContainer}>
-        <Image source={PlaceholderImage} style={styles.image} />
+    <View style={ styles.container }>
+      <View style={ styles.imageContainer }>
+        <ImageViewer imageSource={ PlaceholderImage } />
       </View>
     </View>
   );
@@ -20,11 +21,6 @@ const styles = StyleSheet.create({
     alignItems: 'center',
   },
   imageContainer: {
-    flex: 1,    
-  },
-  image: {
-    width: 320,
-    height: 440,
-    borderRadius: 18,
+    flex: 1,
   }
 });
