817666526584e16b96b42841ea7b5e2fed73cae0
Ashwin Poefra <kaspersky3550879@gmail.com>
2024-11-12 08:41:22 +0900

    feat: add get current location function

--- app/(tabs)/location.tsx
+++ app/(tabs)/location.tsx
@@ -1,21 +1,32 @@
+import * as Location from 'expo-location';
+import React, { useState, useEffect } from 'react';
 import { Text, View, StyleSheet } from 'react-native';
+import { ALERT_TYPE, Dialog, AlertNotificationRoot, Toast } from 'react-native-alert-notification';
 
 export default function LocatinoScreen() {
+  const [location, setLocation] = useState(null);
+
+  useEffect(() => {
+    (async () => {
+      let { status } = await Location.requestForegroundPermissionsAsync();
+
+      if (status !== 'granted') {
+        Toast.show({
+          type: ALERT_TYPE.WARNING,
+          title: 'Warn',
+          textBody: 'Permission to access location was denied!',
+        });
+      }
+
+      let currentLocation = await Location.getCurrentPositionAsync({});
+      setLocation(currentLocation);
+    })();
+  }, []);
   return (
-    <View style={styles.container}>
-      <Text style={styles.text}>Location Screen</Text>
+    <View>
+      <Text>
+        Location: {location ? JSON.stringify(location) : 'Fetching location...'}
+      </Text>
     </View>
   )
 }
-
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#25292e',
-    justifyContent: 'center',
-    alignItems: 'center',
-  },
-  text: {
-    color: '#fff',
-  }
-})
